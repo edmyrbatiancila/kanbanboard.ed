e30fcf1f25c1ae0e5dab754e1ebfcc3eb0ca308e
edmyrbatiancila <batiancilaedmyr@gmail.com>
2024-09-30 02:31:19 +0800

    update files

--- kanban-script.js
+++ kanban-script.js
@@ -4,14 +4,33 @@ $(document).ready(function() {
     $(".drop-task").droppable({
         drop: function(event, ui) {
             $(".drag-task").draggable("option", "revert", false);
-        }
-    });
 
-    $(".drop_todo").droppable({
-        drop: function(event, ui) {
-            let currentElement = $('span').siblings().attr('title');
+            let dropSection = $(this).parent().attr("id");
 
-            $("drag-task").find(currentElement).css("background-color", "blue");
+            let color;
+
+            switch(dropSection) {
+                case 'todo':
+                    color = 'blue';
+                    break;
+                case 'in-progress':
+                    color = 'orange';
+                    break;
+                case 'for-approval':
+                    color = 'pink';
+                    break;
+                case 'testing':
+                    color = 'violet';
+                    break;
+                case 'done':
+                    color = 'green';
+                    break;
+                default :
+                    color = 'white';
+            }
+
+            $(ui.draggable).css("background-color", color)
+                            .css("color", "white");
         }
     });
 });
